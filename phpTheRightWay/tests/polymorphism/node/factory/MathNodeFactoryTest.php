34adc9028173c9ab46c362eae8fdee7c57aeaa40
EleganteVagbond <hpogt1@gmail.com>
2020-12-15 19:31:50 -0700

    test coverage for invalid operators and values

--- phpTheRightWay/tests/polymorphism/node/factory/MathNodeFactoryTest.php
+++ phpTheRightWay/tests/polymorphism/node/factory/MathNodeFactoryTest.php
@@ -2,86 +2,94 @@
 
 namespace Polymorphism\Node\Factory;
 
+use InvalidArgumentException;
 use \Polymorphism\Node\Factory\MathNodeFactory;
 use PHPUnit\Framework\TestCase;
 use Polymorphism\Node\OperatorNode;
 
 final class MathNodeFactoryTest extends TestCase
 {
+    protected $factory;
+
+    protected function setUp() : void {
+        $this->factory = new MathNodeFactory();
+
+    }
     public function testCanBeCreated() : void {
         $this->assertInstanceOf(MathNodeFactory::class, new MathNodeFactory());
     }
 
     public function testCanParseOneAdditionOperation() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("1 + 1");
+        $node = $this->factory->parse("1 + 1");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(2,$node->evaluate());
     }
 
     public function testCanParseTwoAdditionOperations() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("1 + 1 + 1");
+        $node = $this->factory->parse("1 + 1 + 1");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(3,$node->evaluate());
     }
 
     public function testCanParseOneMultiplicationOperation() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("1 * 5");
+        $node = $this->factory->parse("1 * 5");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(5,$node->evaluate());
     }
 
     public function testCanParseTwoMultiplicationOperations() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("1 * 2 * 3");
+        $node = $this->factory->parse("1 * 2 * 3");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(6,$node->evaluate());
     }
 
     public function testCanParseOneDivisionOperation() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("4 / 2");
+        $node = $this->factory->parse("4 / 2");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(2,$node->evaluate());
     }
 
     public function testCanParseTwoDivisionOperations() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("8 / 8 / 2");
+        $node = $this->factory->parse("8 / 8 / 2");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(2,$node->evaluate());
     }
 
     public function testCanParseOneSubtractionOperation() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("4 - 2");
+        $node = $this->factory->parse("4 - 2");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(2,$node->evaluate());
     }
 
     public function testCanParseTwoSubtractionOperations() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("8 - 4 - 2");
+        $node = $this->factory->parse("8 - 4 - 2");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(6,$node->evaluate());
     }
 
     public function testCanParseMixedOperations() : void {
-        $factory = new MathNodeFactory();
-        $node = $factory->parse("8 / 4 - 2");
+        $node = $this->factory->parse("8 / 4 - 2");
         $this->assertNotNull($node);
         $this->assertInstanceOf(OperatorNode::class, $node);
         $this->assertEquals(4,$node->evaluate());
+    }
+
+    public function testHandlesInvalidOperators() : void {
+        $this->expectException(InvalidArgumentException::class);
+        $this->factory->parse("5 ^ 3");
+    }
+
+    public function testHandlesInvalidValues() : void {
+        $this->expectException(InvalidArgumentException::class);
+        $this->factory->parse("a * b");
     }
 
 }
